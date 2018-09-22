7e1328a44096ca215df7c7aa31899c6d7ef14c5b
Charles EDOU NZE <charles@charlesen.fr>
2018-09-22 17:34:21 +0200

    Sorting + local date format

--- tweet/static/js/twitter.vue.js
+++ tweet/static/js/twitter.vue.js
@@ -8,17 +8,47 @@ Vue.http.headers.common['X-CSRFToken'] = "{{ csrf_token }}";
         currentTweet: {},
         message: null,
         newTweet: { 'tweet_name': null, 'tweet_message': null },
-        search_term: '',
+        maxCount: 50,
+        remainingCount: 50,
+        hasError: false,
+        currentSort:'tweet_name',
+        currentSortDir:'asc'
       },
       mounted: function() {
         this.getTweets();
       },
+      computed:{
+        sortedTweets:function() {
+          return this.tweets.sort((a,b) => {
+
+            console.log('a', a[this.currentSort]);
+            console.log('b', b[this.currentSort]);
+
+            let modifier = 1;
+            if(this.currentSortDir === 'desc') modifier = -1;
+            if(a[this.currentSort] < b[this.currentSort]) return -1 * modifier;
+            if(a[this.currentSort] > b[this.currentSort]) return 1 * modifier;
+            return 0;
+          });
+        }
+      },
       methods: {
+        sort:function(s) {
+          if(s === this.currentSort) {
+            this.currentSortDir = this.currentSortDir==='asc'?'desc':'asc';
+          }
+          this.currentSort = s;
+        },
+        countdown: function() {
+          let currentLen = this.newTweet.tweet_message ? this.newTweet.tweet_message.length : this.currentTweet.tweet_message.length;
+          this.remainingCount = this.maxCount - currentLen;
+          this.hasError = this.remainingCount < 0;
+        },
+        formatDate(d) {
+          return new Date(d).toLocaleString();
+        },
         getTweets: function() {
           let api_url = '/api/tweet/';
-          if(this.search_term!==''||this.search_term!==null) {
-            api_url = `/api/tweet/?search=${this.search_term}`
-          }
           this.loading = true;
           this.$http.get(api_url)
               .then((response) => {
